22d0f118571fa2749fbdc85bc0e13c39a7bbf265
Antonio Colasurdo <antonio.colasurdo98@gmail.com>
2021-01-24 10:31:41 +0100

    Preparazione infrastruttura iniziale del progetto

---
+++ src/main/java/it/univpm/progetto/UnivpmProgrammazioneAdOggettiApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivpmProgrammazioneAdOggettiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivpmProgrammazioneAdOggettiApplication.class, args);
+	}
+
+}
